557f7aad0e97b6fbb39c9182c1056f40de6a8424
yaseenaiman <yaseenaiman17@gmail.com>
2022-07-03 12:53:57 +0300

    Updates

---
+++ precache-manifest.78a845ad7f40af145cc383b206c5ac6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fcb08f40445f212b50e",
+    "url": "/static/css/main.342803ed.chunk.css"
+  },
+  {
+    "revision": "3fcb08f40445f212b50e",
+    "url": "/static/js/main.3fcb08f4.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "142a3514bd807649de2f",
+    "url": "/static/css/2.f0efe9d6.chunk.css"
+  },
+  {
+    "revision": "142a3514bd807649de2f",
+    "url": "/static/js/2.142a3514.chunk.js"
+  },
+  {
+    "revision": "82555cf690c0363f7ef9",
+    "url": "/static/js/runtime~main.82555cf6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4bc7c923a75affc1667136781878f83b",
+    "url": "/index.html"
+  }
+];
